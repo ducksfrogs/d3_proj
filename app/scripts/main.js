715454475ab6db212eaf43fbece73c882de9f9f3
ma <ma_yamaki@yahoo.com>
2021-02-11 22:39:22 +0900

    dd

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,9 +9,25 @@ console.log(a);
 //add an svg element 
 const svg = canva.append('svg')
                 .attr('width', '800')
-                .attr('height', '500');
+                .attr('height', '600');
 svg.append('circle').attr("cx", "340")
                     .attr("cy", "34")
                     .attr('r', "50")
                     .attr('fill', "Blue");
 
+
+svg.append('rect').attr('width', '10')
+                .attr('height', '40')
+                .attr('x', '40')
+                .attr('y', '50');
+
+
+svg.append('rect').attr('width', '100')
+                .attr('height', '100')
+                .attr('x', '40')
+                .attr('y', '50')
+                .attr('rx', 15);
+
+svg.append('text').text("hello there")
+            .attr("x",100)
+            .attr('y', 300)
